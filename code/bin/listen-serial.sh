55ea508d9ade1da748f54313fa08e7de7d6a43fc
Turbo Fredriksson <turbo@bayour.com>
2024-06-27 19:17:23 +0100

    Run `defmt-print` in the listen serial script with the verbose flag.

--- code/bin/listen-serial.sh
+++ code/bin/listen-serial.sh
@@ -5,4 +5,4 @@ export DEV="$(echo /dev/cu.usbmodem*)"
 
 # On MacOS, the `stty` needs to come *after* the port is opened!
 (cat < "${DEV}" & stty -f "${DEV}" speed 115200 -crtscts -mdmbuf) | \
-    defmt-print -e ./target.elf
+    defmt-print -e ./target.elf --verbose --show-skipped-frames stdin
