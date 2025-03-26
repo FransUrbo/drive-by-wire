0017b56ce2202ad66a9c1115fb3ecb41db39fb0c
Turbo Fredriksson <turbo@bayour.com>
2025-03-26 13:31:01 +0000

    Rewrite the actuator test binary.
    * Change the way we test the actuator.
      1. Move the actuator to the closest (well .. :) endpoint.
      2. Move the actuator all the way to the other endpoint.
      3. Move the actuator 10mm at a time, 10 times.
    + Actuator public statics have changed names, so update code.
    - Disable the serial output (for now).

--- code/src/test-actuator.rs
+++ code/src/test-actuator.rs
@@ -12,12 +12,12 @@ use embassy_rp::peripherals::UART1;
 use embassy_rp::uart::{
     Blocking, Config as UartConfig, InterruptHandler as UARTInterruptHandler, UartTx,
 };
-use embassy_time::Timer;
 
-use actuator::{Actuator, Direction, THROW_TIME_PER_1MM};
+use actuator::{Actuator, Direction, RESISTANCE_THROW_MIN, TIME_THROW_1MM, TIME_THROW_TOTAL};
 use static_cell::StaticCell;
 
-use {defmt_serial as _, panic_probe as _};
+//use {defmt_serial as _, panic_probe as _};
+use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
     UART1_IRQ    => UARTInterruptHandler<UART1>;	// Serial logging
@@ -31,9 +31,9 @@ async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
     // Initialize the serial UART for debug/log output.
-    let uart = UartTx::new(p.UART1, p.PIN_4, p.DMA_CH4, UartConfig::default()); // => 115200/8N1
-    static SERIAL: StaticCell<UartTx<'_, UART1, Blocking>> = StaticCell::new();
-    defmt_serial::defmt_serial(SERIAL.init(uart));
+//    let uart = UartTx::new(p.UART1, p.PIN_4, p.DMA_CH4, UartConfig::default()); // => 115200/8N1
+//    static SERIAL: StaticCell<UartTx<'_, UART1, Blocking>> = StaticCell::new();
+//    defmt_serial::defmt_serial(SERIAL.init(uart));
 
     // Initialize the actuator.
     let mut actuator = Actuator::new(
@@ -45,23 +45,20 @@ async fn main(_spawner: Spawner) {
         Irqs,
     );
 
-    // 5s should be enough to move it from one end to another..
-    let move_distance = 5000; // ms
-    let mut direction = Direction::Forward;
+    let pot = actuator.read_pot().await;
+    debug!("Initial actuator position value: {:?}", pot);
+    let mut direction: Direction = Direction::Backward;
+    if (pot > (RESISTANCE_THROW_MIN - 200)) && (pot < (RESISTANCE_THROW_MIN + 200)) {
+        direction = Direction::Forward;
+    }
 
-    // Move the actuator to the forward-most position before we begin.
-    debug!("Move the actuator to the forward-most position before we begin");
-    actuator.move_actuator(move_distance, direction).await;
-    Timer::after_secs(1).await; // Give it a second to "settle". Just in case..
-
+    // 1. Move the actuator to the closest (well .. :) endpoint.
+    // 2. Move the actuator all the way to the other endpoint.
+    // 3. Move the actuator 10mm at a time, 10 times.
     loop {
-        // =====
-        // Reverse the direction.
-        if direction == Direction::Backward {
-            direction = Direction::Forward;
-        } else {
-            direction = Direction::Backward;
-        }
+	// Move the actuator to the outward-most position before we begin.
+	debug!("Move the actuator to the outward-most position before we begin");
+	actuator.move_actuator(TIME_THROW_TOTAL, direction).await;
 
         // Read the start position value.
         info!(
@@ -69,9 +66,15 @@ async fn main(_spawner: Spawner) {
             actuator.read_pot().await
         );
 
-        // Move it all the way to the backward-most position.
-        actuator.move_actuator(move_distance, direction).await;
-        Timer::after_secs(1).await; // Give it a second to "settle". Just in case..
+        // Reverse the direction.
+        if direction == Direction::Backward {
+            direction = Direction::Forward;
+        } else {
+            direction = Direction::Backward;
+        }
+
+        // Move it all the way to the other outer-most position.
+        actuator.move_actuator(TIME_THROW_TOTAL, direction).await;
 
         // Read the end position value.
         info!(
@@ -87,38 +90,14 @@ async fn main(_spawner: Spawner) {
             direction = Direction::Backward;
         }
 
-        // Move the actuator ten times 10mm.
-        info!("Moving actuator 1s at a time");
-        for i in 1..=10 {
-            info!("Move={}", i);
-
-            actuator.move_actuator(1000, direction).await;
-            Timer::after_secs(1).await; // Give it a second to "settle". Just in case..
-
-            info!(
-                "Actuator potentiometer value (#3/{}): {}Ω",
-                i,
-                actuator.read_pot().await
-            );
-        }
-
-        // =====
-        // Reverse the direction.
-        if direction == Direction::Backward {
-            direction = Direction::Forward;
-        } else {
-            direction = Direction::Backward;
-        }
-
         // Move the actuator 10mm at a time
         info!("Moving actuator 10mm at a time");
         for i in 1..=10 {
             info!("Move={}", i);
 
             actuator
-                .move_actuator((THROW_TIME_PER_1MM as u64) * 10, direction)
+                .move_actuator((TIME_THROW_1MM as u64) * 10, direction)
                 .await;
-            Timer::after_secs(1).await; // Give it a second to "settle". Just in case..
 
             info!(
                 "Actuator potentiometer value (#3/{}): {}Ω",
