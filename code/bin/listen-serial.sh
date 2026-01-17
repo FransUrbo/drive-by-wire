229f97803bcff2d6d9e077680904402656925839
FransUrbo <turbo@bayour.com>
2026-01-17 21:47:04 +0000

    Move the output from the `listen-serial.sh` script to `./logs`.

--- code/bin/listen-serial.sh
+++ code/bin/listen-serial.sh
@@ -3,11 +3,13 @@
 # This is the device for my Debug Probe on my MacOS host.
 export DEV="$(echo /dev/cu.usbmodem*)"
 
+[ ! -d "logs" ] && mkdir logs
+
 # On MacOS, the `stty` needs to come *after* the port is opened!
 (cat < "${DEV}" & /bin/stty -f "${DEV}" speed 115200 -crtscts -mdmbuf) | \
     defmt-print -e ./target.elf --verbose --show-skipped-frames stdin | \
-    tee -p run.log-raw | \
+    tee -p logs/run.log-raw | \
     grep --line-buffered -v '^└─ ' | \
-    tee -p run.log-filtered | \
+    tee -p logs/run.log-filtered | \
     grep --line-buffered ' INFO ' | \
-    tee -p run.log-info
+    tee -p logs/run.log-info
