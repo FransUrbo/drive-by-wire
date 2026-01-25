8598ce3af02e521bc9f685265ec8fbc6de46c685
FransUrbo <turbo@bayour.com>
2026-01-25 16:59:20 +0000

    Tweak the run-bin script.
    Get the `DEFMT_LOG` variable the code was built with last if possible.
    I like to run through it with `cargo build` first, just to make sure.
    I set that variable differently, depending on what I want to work on.
    
    Without it, `cargo run` will build all of it again, from scratch,
    which is just time consuming..

--- code/bin/run_bin.sh
+++ code/bin/run_bin.sh
@@ -18,6 +18,24 @@ else
     BIN="${1}"
 fi
 
+# Try to figure out log level from already built binary.
+# I like to double check with `cargo build` before I run it :).
+if [ -d "target" ]; then
+    b="$(echo "${BIN}" | sed 's@-@_@g')" # Cargo replaces all dashes in bin names with slashes.
+    files="$(find target/thumbv6m-none-eabi/debug/deps/ -type f -name "${b}*.d")"
+
+    unset ENVS
+    for file in ${files}; do
+	if [ -n "${ENVS}" ]; then
+	    ENVS="${ENVS}
+$(grep 'env-dep:' "${file}" | sed 's@.*:@@')"
+	else
+	    ENVS="$(grep 'env-dep:' "${file}" | sed 's@.*:@@')"
+	fi
+    done
+fi
+
+eval "${ENVS}" # Safe even if it's unset.
 cargo run --bin "${BIN}" | \
     tee -p logs/run.log-raw | \
     grep --line-buffered -v '^└─ '
