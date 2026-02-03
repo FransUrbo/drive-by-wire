8ab15cb9e4a76f8ddd1d9e534765a72158f30bf0
FransUrbo <turbo@bayour.com>
2026-02-03 22:04:55 +0000

    Additional options to listen on the serial port added.

--- code/bin/listen-serial.sh
+++ code/bin/listen-serial.sh
@@ -5,6 +5,10 @@ export DEV="$(echo /dev/cu.usbmodem*)"
 
 [ ! -d "logs" ] && mkdir logs
 
+# OR:
+# minicom -b 115200 -o -D "${DEV}"
+# screen "${DEV}" 115200
+
 # On MacOS, the `stty` needs to come *after* the port is opened!
 (cat < "${DEV}" & /bin/stty -f "${DEV}" speed 115200 -crtscts -mdmbuf) | \
     defmt-print -e ./target.elf --verbose --show-skipped-frames stdin | \
