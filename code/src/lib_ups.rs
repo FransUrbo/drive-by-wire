a050e4b01e84898c57514b2fb47674c5a69b0520
FransUrbo <turbo@bayour.com>
2026-02-04 08:39:15 +0000

    Calculate how much charge in the UPS is left.

--- code/src/lib_ups.rs
+++ code/src/lib_ups.rs
@@ -29,17 +29,26 @@ pub async fn ups_monitor(ups: PeriPowerMonitor) {
     loop {
         let measurement = ina.next_measurement().unwrap().expect("A measurement is ready");
 
-        debug!("Voltage (Bus):   {=f32:#02} V",
+        // Calculate how much charge is left.
+        let mut charge: f32 = ((measurement.bus_voltage.voltage_mv() - 3) as f32) / 1.2 * 100.0;
+        if charge < 0.0 {
+            charge = 0.0;
+        } else {
+            charge = 100.0;
+        }
+
+        debug!("Voltage (Bus):   {=f32:#02}V",
               measurement.bus_voltage.voltage_mv() as f32 / 1000.0
         );
-        debug!("Voltage (Shunt): {=f32:#02} mV",
+        debug!("Voltage (Shunt): {=f32:#02}mV",
               measurement.shunt_voltage.shunt_voltage_mv() as f32
         );
         debug!("Power:           {}", measurement.power);
         debug!("Current:         {}", measurement.current);
+        debug!("Charge:          {}%", charge);
 
-        // Checking every 15s should be more than enough.
-        Timer::after_secs(15).await;
+        // Checking every minute should be more than enough.
+        Timer::after_secs(60).await;
     }
 }
 
