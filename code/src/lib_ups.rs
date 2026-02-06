14e29d25fe90516d8c058ed7d409874c08551561
FransUrbo <turbo@bayour.com>
2026-02-06 17:25:04 +0000

    Rearrange the UPS code and add debug if/when on battery power.

--- code/src/lib_ups.rs
+++ code/src/lib_ups.rs
@@ -23,7 +23,10 @@ bind_interrupts!(struct Irqs {
 pub async fn ups_monitor(ups: PeriPowerMonitor) {
     let i2c = i2c::I2c::new_async(ups.i2c, ups.scl, ups.sda, Irqs, i2c::Config::default());
 
-    let calib = IntCalibration::new(MicroAmpere(1_000_000), 1_000).unwrap();
+    // Resolution of 1A, and a shunt of 10mΩ.
+    // The shunt resistor in the Pico UPS Hat B: R1/0.01Ω (10,000µΩ/10mΩ).
+    //let calib = IntCalibration::new(MicroAmpere(1_000_000), 10_000).unwrap();
+    let calib = IntCalibration::new(MicroAmpere(100), 10_000).unwrap();
     let mut ina = SyncIna219::new_calibrated(i2c, Address::from_byte(0x43).unwrap(), calib).unwrap();
 
     loop {
@@ -37,15 +40,24 @@ pub async fn ups_monitor(ups: PeriPowerMonitor) {
             charge = 100.0;
         }
 
-        debug!("Voltage (Bus):   {=f32:#02}V",
-              measurement.bus_voltage.voltage_mv() as f32 / 1000.0
-        );
-        debug!("Voltage (Shunt): {=f32:#02}mV",
-              measurement.shunt_voltage.shunt_voltage_mv() as f32
-        );
         debug!("Power:           {}", measurement.power);
         debug!("Current:         {}", measurement.current);
         debug!("Charge:          {}%", charge);
+
+        debug!("Voltage (Bus):   {=f32:#02}V",
+              measurement.bus_voltage.voltage_mv() as f32 / 1000.0
+        );
+
+        let shunt_voltage_mv = measurement.shunt_voltage.shunt_voltage_mv();
+        let shunt_voltage_uv = measurement.shunt_voltage.shunt_voltage_uv();
+        debug!("Voltage (Shunt): {=f32:#02}mV ({=f32:#02}µV)",
+              shunt_voltage_mv as f32,
+              shunt_voltage_uv as f32,
+        );
+
+        if (shunt_voltage_uv as i16) < -350 {
+            debug!("=> On battery");
+        }
 
         // Checking every minute should be more than enough.
         Timer::after_secs(60).await;
